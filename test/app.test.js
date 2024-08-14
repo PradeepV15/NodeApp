7d6fd34bdea866732b7e0ac01ccc3826b0bffccf
PradeepV15 <pradeep.v@commerceiq.ai>
2024-08-14 11:20:58 +0530

    workflow change

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,6 @@ const app = require("../index"); // Assuming your index.js exports the app
 describe("GET /", () => {
   it("should return Hello, world!", async () => {
     const res = await request(app).get("/");
-    expect(res.text).toBe("Hello, world!");
+    expect(res.text).toBe("Hello, earth!");
   });
 });
